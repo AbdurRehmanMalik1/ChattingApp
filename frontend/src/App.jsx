2d5c096e018237b5b2ef2a665087f97e8a0d858d
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2025-01-14 18:54:36 +0500

    Chat page progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import './css/App.css'
+import Chat from "./components/Chat";
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(true); 
@@ -9,14 +10,39 @@ const App = () => {
     setIsLogin(!isLogin);
   };
 
+  const chats = [];
+  async function getUserChats(){
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzdlNDdlYjQ3YzQ3YmE3YTM5YjlhMmUiLCJuYW1lIjoiQWJkdXIgUmVobWFuIiwiZW1haWwiOiJhYmR1cnJlaG1hbjQ0MTVAZ21haWwuY29tIiwiaWF0IjoxNzM2ODYyNzUzLCJleHAiOjE3MzY4Njk5NTN9.Ts7OIvl3rQ7VCjrgJiT54PMrshKCe76mOedezda1aBQ";
+    const bearerToken = `Bearer ${token}`
+    try {
+        const response = await fetch('http://localhost:8080/chat/my', {
+          method: "GET",
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': bearerToken
+          }
+        });
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching user chats:", error);
+      }
+  }
+  getUserChats();
   return (
-    <div className="app-div">
-        {isLogin ? (
-          <Login toggleForm={toggleForm} />
-        ) : (
-          <Signup toggleForm={toggleForm} />
-        )}
-    </div>
+    // <div className="app-div">
+    //     {isLogin ? (
+    //       <Login toggleForm={toggleForm} />
+    //     ) : (
+    //       <Signup toggleForm={toggleForm} />
+    //     )}
+    // </div>
+
+    <Chat/>
   );
 };
 
---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Chat(){
+    return(
+        <>
+            <h1>this is the chatting page</h1>
+        </>
+    );
+}
+
+
+
+
+export default Chat
